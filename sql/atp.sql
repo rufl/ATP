94b3e58b94ab3fb51ad266f5852b4c4c76622049
Rafael Dina <rafael.dina@gmail.com>
2012-02-17 19:27:03 -0200

    0.75

---
+++ sql/atp.sql
@@ -0,0 +1,16 @@
+-- --------------------------------------------------------
+-- Host:                         192.168.56.101
+-- Server version:               5.0.77 - Source distribution
+-- Server OS:                    redhat-linux-gnu
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2012-02-17 19:23:36
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+-- Data exporting was unselected.
+
+-- Data exporting was unselected.
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
